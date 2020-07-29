b7a89c44bcda8673b0651b2b4b30e471e997871e
Sion Kim <kim.gloria0417@gmail.com>
2020-07-29 11:16:14 -0400

    Commented out messing around on routing with signed in/out functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  if signed_in?
+  #if signed_in?
     root 'navigation#index' #setting root page as navigation
-  else
-    root 'general#index'
-  end
+  #else
+#    root 'general#index'
+#  end
 
   get '/' => 'navigation#index', as: 'navigation'
   get '/practice' => 'practice#index', as: 'practice'
