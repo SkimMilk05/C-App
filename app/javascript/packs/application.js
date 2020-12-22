d05f22908c7e2e7f41fcb465ff03ccf103b1e707
Sion Kim <sk9uth@virginia.edu>
2020-12-21 21:19:47 -0500

    commented out webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+/*
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -11,7 +11,7 @@ require("channels")
 require("jQuery")
 
 import "scripts" //to use my custom scripts
-
+*/
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
